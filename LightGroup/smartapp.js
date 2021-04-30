2d5eaeef9acadcdbf6807a0c4389a898da7b066e
davidhkates <davidhkates@yahoo.com>
2021-04-29 17:52:48 -0700

    Update smartapp.js

--- LightGroup/smartapp.js
+++ LightGroup/smartapp.js
@@ -62,7 +62,7 @@ module.exports = new SmartApp()
 
     // Turn on the lights when main switch is pressed
     .subscribedEventHandler('mainSwitchOnHandler', async (context, event) => {
-        await context.api.devices.sendCommands(context.config.onGroup, 'switch', 'on');
+        // await context.api.devices.sendCommands(context.config.onGroup, 'switch', 'on');
     
         // Turn on the lights in the on group if they are all off
         const stateSwitches = onGroup.map(it => context.api.devices.getCapabilityStatus(
