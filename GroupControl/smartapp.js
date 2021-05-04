15965f958f7369601732fdba07c01446281c4877
davidhkates <davidhkates@yahoo.com>
2021-05-04 04:13:29 -0700

    Update smartapp.js

--- GroupControl/smartapp.js
+++ GroupControl/smartapp.js
@@ -1,9 +1,11 @@
 const SmartApp   = require('@smartthings/smartapp');
+const contextStore = new DynamoDBContextStore({AWSRegion: 'us-west-2'});
 
 /* Define the SmartApp */
 module.exports = new SmartApp()
     .enableEventLogging()  // logs requests and responses as pretty-printed JSON
     .configureI18n()        // auto-create i18n files for localizing config pages
+    .contextStore(contextStore)     // context store to persist room state
 
     // Configuration page definition
     .page('mainPage', (context, page, configData) => {
