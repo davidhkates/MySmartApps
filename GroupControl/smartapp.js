90b15dd57aedaa649fe82d2037acf69b627a37d1
davidhkates <davidhkates@yahoo.com>
2021-05-07 12:17:03 -0700

    Update smartapp.js

--- GroupControl/smartapp.js
+++ GroupControl/smartapp.js
@@ -47,7 +47,7 @@ const contextStore = new DynamoDBContextStore({
 module.exports = new SmartApp()
     .enableEventLogging()  // logs requests and responses as pretty-printed JSON
     .configureI18n()        // auto-create i18n files for localizing config pages
-    // .contextStore(contextStore)     // context store to persist room state
+    .contextStore(contextStore)     // context store to persist room state
 
     // Configuration page definition
     .page('mainPage', (context, page, configData) => {
