cf6d212c61617b498cf2b52acb7b50ae51e72f6e
davidhkates <davidhkates@yahoo.com>
2021-05-11 10:36:42 -0700

    Moved state-variable.js to Utilities

--- ShadeControl/smartapp.js
+++ ShadeControl/smartapp.js
@@ -1,5 +1,6 @@
 const SmartApp   = require('@smartthings/smartapp');
-const stateVariable = require('./state-variable');
+// const stateVariable = require('./state-variable');
+const stateVariable = require('../Utilities/state-variable');
 
 /* Define the SmartApp */
 module.exports = new SmartApp()
