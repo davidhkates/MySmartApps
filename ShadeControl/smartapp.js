4c2bfbaae11287159c1967a9c1f1401cb86d7764
davidhkates <davidhkates@yahoo.com>
2021-05-14 16:51:05 -0700

    Update smartapp.js

--- ShadeControl/smartapp.js
+++ ShadeControl/smartapp.js
@@ -1,6 +1,6 @@
 const SmartApp   = require('@smartthings/smartapp');
 // const stateVariable = require('./state-variable');
-const SmartApp   = require('@davidhkates/dynamo-context');
+const Context   = require('@davidhkates/dynamo-context');
 
 /* Define the SmartApp */
 module.exports = new SmartApp()
