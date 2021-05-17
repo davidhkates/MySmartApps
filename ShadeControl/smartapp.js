eda9322ebe7d0194a47f0f36572d0c5d0e8ab454
davidhkates <davidhkates@yahoo.com>
2021-05-17 16:04:18 -0700

    Update smartapp.js

--- ShadeControl/smartapp.js
+++ ShadeControl/smartapp.js
@@ -1,173 +1,146 @@
 const SmartApp   = require('@smartthings/smartapp');
-// const stateVariable = require('./state-variable');
 const stateVariable = require('@katesthings/smartstate');
 
 /* Define the SmartApp */
 module.exports = new SmartApp()
-    .enableEventLogging()  // logs requests and responses as pretty-printed JSON
-    .configureI18n()        // auto-create i18n files for localizing config pages
-    // .contextStore(contextStore)     // context store to persist room state
+	.enableEventLogging()  // logs requests and responses as pretty-printed JSON
+	.configureI18n()        // auto-create i18n files for localizing config pages
 
-    // Configuration page definition
-    .page('mainPage', (context, page, configData) => {
+	// Configuration page definition
+    	.page('mainPage', (context, page, configData) => {
 
-        // main control switch
-        page.section('switch', section => {
-            section
-                .deviceSetting('shadeControl')
-                .capabilities(['button'])
-                .required(true)
-                .permissions('rx')
-/*
-	    section
-	    	.deviceSetting('shadeDirection')
-		.capabilities(['switch'])
-		.required(false)
-		.permissions('rx');
-*/
-	});
+	// main control switch
+        	page.section('switch', section => {
+			section
+				.deviceSetting('shadeControl')
+				.capabilities(['button'])
+				.required(true)
+				.permissions('rx')
+		});
 
         // shade states
-        page.section('shades', section => {
-            section
-                .deviceSetting('shade0')
-                .capabilities(['momentary'])
-		.permissions('rx');
-            section
-                .deviceSetting('shade1')
-                .capabilities(['momentary'])
-		.permissions('rx');
-            section
-                .deviceSetting('shade2')
-                .capabilities(['momentary'])
-		.permissions('rx');
-            section
-                .deviceSetting('shade3')
-                .capabilities(['momentary'])
-		.permissions('rx')
-        });
-    })
+		page.section('shades', section => {
+			section
+				.deviceSetting('shade0')
+				.capabilities(['momentary'])
+				.permissions('rx');
+			section
+				.deviceSetting('shade1')
+				.capabilities(['momentary'])
+				.permissions('rx');
+			section
+				.deviceSetting('shade2')
+				.capabilities(['momentary'])
+				.permissions('rx');
+			section
+				.deviceSetting('shade3')
+				.capabilities(['momentary'])
+				.permissions('rx')
+		});
+	})
 
-    // Handler called whenever app is installed or updated
-    // Called for both INSTALLED and UPDATED lifecycle events if there is
-    // no separate installed() handler
-    .updated(async (context, updateData) => {
-	console.log("ShadeControl: Installed/Updated");
+	// Handler called whenever app is installed or updated
+	// Called for both INSTALLED and UPDATED lifecycle events if there is
+	// no separate installed() handler
+	.updated(async (context, updateData) => {
+		console.log("ShadeControl: Installed/Updated");
         
-	// initialize state variable(s)
-	stateVariable.putState( context.event.appId, 'shadeState', '0' );
-	stateVariable.putState( context.event.appId, 'shadeDirection', 'down' );
+		// initialize state variable(s)
+		stateVariable.putState( context.event.appId, 'shadeState', '0' );
+		stateVariable.putState( context.event.appId, 'shadeDirection', 'down' );
 
-	// unsubscribe all previously established subscriptions
-	await context.api.subscriptions.unsubscribeAll();
+		// unsubscribe all previously established subscriptions
+		await context.api.subscriptions.unsubscribeAll();
 
-	// create subscriptions for relevant devices
-	await context.api.subscriptions.subscribeToDevices(context.config.shadeControl,
-            'button', 'button.pushed', 'shadeButtonHandler');
-	await context.api.subscriptions.subscribeToDevices(context.config.shadeControl,
-            'switch', 'switch.on', 'shadeUpHandler');
-        await context.api.subscriptions.subscribeToDevices(context.config.shadeControl,
-            'switch', 'switch.off', 'shadeDownHandler');
+		// create subscriptions for relevant devices
+		await context.api.subscriptions.subscribeToDevices(context.config.shadeControl,
+		    'button', 'button.pushed', 'shadeButtonHandler');
+		await context.api.subscriptions.subscribeToDevices(context.config.shadeControl,
+		    'switch', 'switch.on', 'shadeUpHandler');
+		await context.api.subscriptions.subscribeToDevices(context.config.shadeControl,
+		    'switch', 'switch.off', 'shadeDownHandler');
 
-/*
-	await context.api.subscriptions.subscribeToDevices(context.config.shadeDirection,
-            'switch', 'switch.on', 'shadeDirectionUpHandler');
-        await context.api.subscriptions.subscribeToDevices(context.config.shadeDirection,
-            'switch', 'switch.off', 'shadeDirectionDownHandler');
-
-	await context.api.subscriptions.subscribeToDevices(context.config.shadeControl,
-            'button', 'button.up', 'shadeUpHandler');
-        await context.api.subscriptions.subscribeToDevices(context.config.shadeControl,
-            'button', 'button.down', 'shadeDownHandler');
-*/
-	
-	console.log('Shade Control: END CREATING SUBSCRIPTIONS')
-    })
+		console.log('Shade Control: END CREATING SUBSCRIPTIONS')
+	})
 
 
-    // Update shade state in response to switch being pressed
-    .subscribedEventHandler('shadeButtonHandler', async (context, event) => {
-	// Get session state variable to see if button was manually pressed
-	// console.log("Checking value of mainSwitchPressed");
-	console.log("Shade Button Pushed");
-	// console.log("Context: ", context);
-	console.log("Event: ", event);
+	// Update shade state in response to switch being pressed
+	.subscribedEventHandler('shadeButtonHandler', async (context, event) => {
+		// Get session state variable to see if button was manually pressed
+		// console.log("Checking value of mainSwitchPressed");
+		console.log("Shade Button Pushed");
+		// console.log("Context: ", context);
+		console.log("Event: ", event);
 
-	// determine number of shade states specified
-	var maxState = 3;
-	// while ( ) {
-	//     maxState--;
-	// }
-	       
-	// const oldShadeState = JSON.stringify(await stateVariable.getState( context.event.appId, 'shadeState' )).parseInt();
-	const shadeDirection = await stateVariable.getState( context.event.appId, 'shadeDirection' );
-	const oldShadeState = parseInt( await stateVariable.getState( context.event.appId, 'shadeState' ));
-	var newShadeState = oldShadeState;
-	if ( shadeDirection == "up" ) {
-	    newShadeState = Math.min( oldShadeState+1, maxState ); 
-	} else {
-	    newShadeState = Math.max( oldShadeState-1, 0 );
-        }
-	console.log('Shade state - old: ', oldShadeState, ', new: ', newShadeState);
+		// determine number of shade states specified
+		var maxState = 3;
+		// while ( ) {
+		//     maxState--;
+		// }
 
-	// create shade array
-	const shade_array = [context.config.shade0, context.config.shade1, context.config.shade2, context.config.shade3];
-	
-	// set shade to new state and save in state settings if changed
-	if (newShadeState!=oldShadeState) {	
-		switch(newShadeState) {
-		    case 0:
-			console.log("Pushing button for shade 0");
-			await context.api.devices.sendCommands(context.config.shade0, 'switch', 'on');
-			break;
-		    case "1":
-			console.log("Pushing button for shade 1");
-			await context.api.devices.sendCommands(context.config.shade1, 'switch', 'on');
-			break;
-		    case "2":
-			console.log("Pushing button for shade 2");
-			await context.api.devices.sendCommands(context.config.shade2, 'switch', 'on');
-			break;
-		    case "3":
-			console.log("Pushing button for shade 3");
-			await context.api.devices.sendCommands(context.config.shade3, 'switch', 'on');
-			break;		
-		    default:
-			console.log("We shouldn't ever get here... Old Shade State: ", oldShadeState, ", New Shade State: ", newShadeState);
+		// const oldShadeState = JSON.stringify(await stateVariable.getState( context.event.appId, 'shadeState' )).parseInt();
+		const shadeDirection = await stateVariable.getState( context.event.appId, 'shadeDirection' );
+		const oldShadeState = parseInt( await stateVariable.getState( context.event.appId, 'shadeState' ));
+		var newShadeState = oldShadeState;
+		if ( shadeDirection == "up" ) {
+			newShadeState = Math.min( oldShadeState+1, maxState ); 
+		} else {
+		    	newShadeState = Math.max( oldShadeState-1, 0 );
 		}
-		stateVariable.putState( context.event.appId, 'shadeState', newShadeState.toString() );
-	}	
-    })
+		console.log('Shade state - old: ', oldShadeState, ', new: ', newShadeState);
+
+		// create shade array
+		const shade_array = [context.config.shade0, context.config.shade1, context.config.shade2, context.config.shade3];
+
+		// set shade to new state and save in state settings if changed
+		if (newShadeState!=oldShadeState) {	
+			switch(newShadeState) {
+				case 0:
+					console.log("Pushing button for shade 0");
+					await context.api.devices.sendCommands(context.config.shade0, 'switch', 'on');
+					break;
+				case "1":
+					console.log("Pushing button for shade 1");
+					await context.api.devices.sendCommands(context.config.shade1, 'switch', 'on');
+					break;
+				case "2":
+					console.log("Pushing button for shade 2");
+					await context.api.devices.sendCommands(context.config.shade2, 'switch', 'on');
+					break;
+				case "3":
+					console.log("Pushing button for shade 3");
+					await context.api.devices.sendCommands(context.config.shade3, 'switch', 'on');
+					break;		
+				default:
+					console.log("We shouldn't ever get here... Old Shade State: ", oldShadeState, ", New Shade State: ", newShadeState);
+			}
+			stateVariable.putState( context.event.appId, 'shadeState', newShadeState.toString() );
+		}	
+	})
 
 
-    .subscribedEventHandler('shadeUpHandler', async (context, event) => {
-	// Get session state variable to see if button was manually pressed
-	// console.log("Checking value of mainSwitchPressed");
-	console.log("On Switch Pressed");
-	// console.log("Context: ", context);
-	// console.log("Event: ", event);
-
-	// await context.api.devices.sendCommands(context.config.switch0, 'switch', 'on');
-	console.log("Set shade to state 2");
-	await context.api.devices.sendCommands(context.config.shade2, 'switch', 'on');
-	console.log("Shade control button pressed");
-        stateVariable.putState( context.event.appId, 'shadeDirection', 'up' );
-    })
+	// When on pressed, set shade direction state variable to "up"
+    	.subscribedEventHandler('shadeUpHandler', async (context, event) => {
+		console.log("On Switch Pressed");
+		stateVariable.putState( context.event.appId, 'shadeDirection', 'up' );
+/*
+		// await context.api.devices.sendCommands(context.config.switch0, 'switch', 'on');
+		console.log("Set shade to state 2");
+		await context.api.devices.sendCommands(context.config.shade2, 'switch', 'on');
+		console.log("Shade control button pressed");
+*/
+	})
 
 
-    .subscribedEventHandler('shadeDownHandler', async (context, event) => {
-	// Get session state variable to see if button was manually pressed
-	// console.log("Checking value of mainSwitchPressed");
-	console.log("Off Switch Pressed");
-	// console.log("Context: ", context);
-	// console.log("Event: ", event);
-
-	stateVariable.putState( context.event.appId, 'shadeDirection', 'down' );
-    });
+	// When off pressed, set shade direction state variable to "down"
+	.subscribedEventHandler('shadeDownHandler', async (context, event) => {
+		console.log("Off Switch Pressed");
+		stateVariable.putState( context.event.appId, 'shadeDirection', 'down' );
+	});
 
 
 /*
-    .subscribedEventHandler('switchHandler', async (ctx, event) => {
-	console.log(`EVENT ${event.deviceId} ${event.componentId}.${event.capability}.${event.attribute}: ${event.value}`)
-    })
+	.subscribedEventHandler('switchHandler', async (ctx, event) => {
+		console.log(`EVENT ${event.deviceId} ${event.componentId}.${event.capability}.${event.attribute}: ${event.value}`)
+	})
 */
