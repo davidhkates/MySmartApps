d971f2d8839556816bfe4d4af068fc12b273f443
davidhkates <davidhkates@yahoo.com>
2021-05-22 05:47:24 -0700

    Cleaned up inTimeWindow

--- FanControl/smartapp.js
+++ FanControl/smartapp.js
@@ -15,17 +15,17 @@ function inTimeWindow( startDateTime, endDateTime ) {
 		// apply current date to start and end date/time
 		const currentDate = new Date();
 		startDateTime.setFullYear( currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() );
-		inTimeWindow = ( currentDate >= startDateTime );
+		endDateTime.setFullYear( currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() );
 
-		if ( endDateTime && inTimeWindow ) {
-			endDateTime.setFullYear( currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() );
-
-			// if times cross midnight, add a day to end date/time
-			if ( startDateTime > endDateTime ) {
-				endDateTime.setDate(endDateTime.getDate() + 1);
-			}
-			inTimeWindow = ( currentDate <= endDateTime );
-		}
+		// check to see if span midnight, if so, adjust basec on current time
+		if ( startDateTime > endDateTime ) {
+			if (currentDate > startDateTime) {
+				endDateTime.setDate(endDateTime.getDate()+1);
+			} else {
+				startDateTime.setDate(startDateTime.getDate()-1)
+				
+		// check to see if current time is between start and end time		
+		inTimeWindow = ( (currentDate >= startDateTime) && (currentDate <= endDateTime) );
 	}
 	return inTimeWindow;
 }
