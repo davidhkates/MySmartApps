ccca4edc960a0983832024edd738346e11b0e3dd
davidhkates <davidhkates@yahoo.com>
2021-05-21 19:22:22 -0700

    Update smartapp.js

--- FanControl/smartapp.js
+++ FanControl/smartapp.js
@@ -18,17 +18,17 @@ function inTimeWindow( startDateTime, endDateTime ) {
 		startDateTime.setFullYear( currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() );
 		inTimeWindow = ( currentDate >= startDateTime );
 
-/*
 		if ( endDateTime && inTimeWindow ) {
 			endDateTime.setFullYear( currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() );
 
 			// if times cross midnight, add a day to end date/time
+/*
 			if ( startDateTime > endDateTime ) {
 				endDateTime.setDate(endDateTime.getDate() + 1);
 			}
+*/
 			inTimeWindow = ( currentDate <= endDateTime );
 		}
-*/
 	}
 	return inTimeWindow;
 }
