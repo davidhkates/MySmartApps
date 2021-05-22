ea0fea56826248e61f1fe518d65bba11cf77a5bc
davidhkates <davidhkates@yahoo.com>
2021-05-22 13:11:47 -0700

    Update smartapp.js

--- MotionControl/smartapp.js
+++ MotionControl/smartapp.js
@@ -82,13 +82,11 @@ module.exports = new SmartApp()
 	// unsubscribe all previously established subscriptions
 	await context.api.subscriptions.unsubscribeAll();
 
-	/*
 	// create subscriptions for relevant devices
 	await context.api.subscriptions.subscribeToDevices(context.config.motion,
 	    'motionSensor', 'motionSensor.active', 'motionStartHandler');
-	await context.api.subscriptions.subscribeToDevices(context.config.motion,
-	    'motionSensor', 'motionSensor.inactive', 'motionStopHandler');
-	*/
+	// await context.api.subscriptions.subscribeToDevices(context.config.motion,
+	//    'motionSensor', 'motionSensor.inactive', 'motionStopHandler');
 	
 	console.log('Motion Group: END CREATING SUBSCRIPTIONS')
 })
