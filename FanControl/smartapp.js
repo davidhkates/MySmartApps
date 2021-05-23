6d440bdff98c22af50533be0b895e852064da93b
davidhkates <davidhkates@yahoo.com>
2021-05-22 17:58:21 -0700

    Update smartapp.js

--- FanControl/smartapp.js
+++ FanControl/smartapp.js
@@ -39,7 +39,7 @@ async function getTemperature( context, sensor ) {
 	try {
 		const sensorDevice = sensor.deviceConfig;
 		const sensorState = await context.api.devices.getCapabilityStatus( sensorDevice.deviceId, sensorDevice.componentId, 'temperatureMeasurement');
-		console.log('Temperature sensor state: ', sensorState.temperature);
+		console.log('Temperature sensor state: ', sensorState.temperature.value);
 		// return sensorState.temperature.value;
 		return sensorState.value;
 	} catch (err) {
