99baa834e351d90662d542b99201df52ce824cf2
davidhkates <davidhkates@yahoo.com>
2021-05-27 05:22:40 -0700

    Update smartapp.js

--- VacancyControl/smartapp.js
+++ VacancyControl/smartapp.js
@@ -55,7 +55,7 @@ module.exports = new SmartApp()
 		section.timeSetting('endTime')
 			.required(false);
 		section.enumSetting('daysOfWeek')
-			.options(['everyday','weekdays','weekends'])
+			.options(['everyday','weekdays','weekend'])
 			.required(true);
 	});
 })
