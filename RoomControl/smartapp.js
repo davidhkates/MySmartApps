29c7a5c4939704bba25ce4a02e0319bcb0ee5d1a
davidhkates <davidhkates@yahoo.com>
2021-05-30 09:30:52 -0700

    Update smartapp.js

--- RoomControl/smartapp.js
+++ RoomControl/smartapp.js
@@ -108,7 +108,7 @@ module.exports = new SmartApp()
 
 	// Turn on room switch(es) if in time window when light switch turned on
 	console.log("Days of week: ", daysOfWeek);
-	SmartUtils.isDayOfWeek(daysOfWeek)) {
+	if (SmartUtils.isDayOfWeek(daysOfWeek)) {
 		console.log("Today is one of days of week, start time: ", startTime);
 		if (!(startTime)&&!(endTime)) || SmartUtils.inTimeWindow(new Date(startTime), new Date(endTime))) {
 			console.log('Turning room switch(es) on');
