b135920113ee1cf94d7608f0baeda61ce89334b5
davidhkates <davidhkates@yahoo.com>
2021-06-01 15:41:10 -0700

    Update handler.js

--- SonosControl/handler.js
+++ SonosControl/handler.js
@@ -3,9 +3,11 @@
 const callback = (event, context, callback) => {
 
   var token = event.authorizationToken;
+  /*
   console.log('Sonos API Oauth Callback token: ', token);
   console.log('Event: ', event);
   console.log('Context: ', context);
+  */
 
   const response = {
     statusCode: 200,
