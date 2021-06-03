89c8825d25e69dcc15e79f8d05e1bf7c1a144c39
davidhkates <davidhkates@yahoo.com>
2021-06-03 16:09:32 -0600

    Update smartapp.js

--- SonosControl/smartapp.js
+++ SonosControl/smartapp.js
@@ -11,7 +11,8 @@ const http  = require('http');
 const https = require('https');
 
 const requestWeather = {
-	hostname: 'api.openweathermap.org',
+	// hostname: 'api.openweathermap.org',
+	hostname: 'secure.google.com',
 	port: 80,
   	path: '/data/2.5/weather?q=Denver&appid=178796e24e49d001f0999f866eb7eb52',
 	method: 'GET'
