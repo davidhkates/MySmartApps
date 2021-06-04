b120bbac7682c7c9c5b580a7415399547b74e189
davidhkates <davidhkates@yahoo.com>
2021-06-03 20:26:16 -0600

    Update smartapp.js

--- SonosControl/smartapp.js
+++ SonosControl/smartapp.js
@@ -18,6 +18,21 @@ const authRedirect = '&redirect_uri=https%3A%2F%2Fm4bm3s9kj5.execute-api.us-west
 const uriSonosAuthRequest = 'https:///login/v3/oauth?response_type=code&state=testState&scope=playback-control-all&client_id=' + authClient + authRedirect;
 const uriSonosCreateToken = 'https:///login/v3/oauth/access?grant_type=authorization_code=' + authCode + authRedirect;
 
+function callURI( uri ) {
+	// const uri = uriRandom;
+	const getData = async uri => {
+  	try {
+		const response = await axios.get(uri);
+		// const data = response.data;
+		console.log('Response from call to random.org: ', response.data);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+	getData(uri);
+};
+
+
 /* Define the SmartApp */
 module.exports = new SmartApp()
 
@@ -61,7 +76,8 @@ module.exports = new SmartApp()
 	const controlEnabled = context.configBooleanValue('controlEnabled');
 	console.log('Control enabled value: ', controlEnabled);
 	if (controlEnabled) {
-		// const uri = 'http://www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new';
+		callURI(uriRandom);
+		/*
 		const uri = uriRandom;
 		const getData = async uri => {
   			try {
@@ -73,6 +89,7 @@ module.exports = new SmartApp()
   			}
 		};
 		getData(uri);
+		*/
 	}
 	
 	console.log('SonosControl: END CREATING SUBSCRIPTIONS')
