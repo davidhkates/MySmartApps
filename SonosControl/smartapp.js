5f6c6b5b2c27a37f7ee9e3b1556577f50637c2c6
davidhkates <davidhkates@yahoo.com>
2021-06-04 11:24:27 -0700

    Update smartapp.js

--- SonosControl/smartapp.js
+++ SonosControl/smartapp.js
@@ -15,12 +15,13 @@ const uriRandom = 'http://www.random.org/integers/?num=1&min=1&max=10&col=1&base
 const uriWeather = 'http://api.openweathermap.org/data/2.5/weather?q=Denver&appid=178796e24e49d001f0999f866eb7eb52';
 
 // const sonosClientID = 'd313a2a0-960e-481f-9fc7-3c02e4366955';
-const sonosClientID = SmartState.getValue( 'smartapp-sonos-speakers', 'clientID' );
-console.log('Client ID - ', sonosClientID);
+const sonosClientID = await SmartState.getValue( 'smartapp-sonos-speakers', 'clientID' );
+console.log('Client ID: ', sonosClientID);
 
 const sonosToken = '';
 // const sonosRedirect = 'https%3A%2F%2Fm4bm3s9kj5.execute-api.us-west-2.amazonaws.com%2Fdev%2Fcallback';
 const sonosRedirect = encodeURI('https://m4bm3s9kj5.execute-api.us-west-2.amazonaws.com/dev/callback');
+console.log('Redirect URI: ', sonosRedirect);
 const authRedirect = '&redirect_uri=https%3A%2F%2Fm4bm3s9kj5.execute-api.us-west-2.amazonaws.com%2Fdev%2Fcallback';
 const uriSonosCreateToken = 'https:///login/v3/oauth/access?grant_type=authorization_code&code=' + sonosToken + '&redirect_uri=' + sonosRedirect;
 
