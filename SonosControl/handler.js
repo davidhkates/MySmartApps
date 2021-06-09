3014b9ea140d700fb75eeb57851f3f0443422e6a
davidhkates <davidhkates@yahoo.com>
2021-06-08 19:53:03 -0700

    Update handler.js

--- SonosControl/handler.js
+++ SonosControl/handler.js
@@ -29,13 +29,13 @@ async function getURI( uri ) {
 	return responseData;
 };
 
-function getToken( uri, token ) {
+function getToken( uri, data, token ) {
 	const bodyParameters = {
 		key: 'value'
 	};
 
 	const config = {
-    		headers: { Basic: 'Standard {' + token + '}' }
+    		headers: { Basic: token }
 	};
 	
 	axios.post(uri, bodyParameters, config).then(console.log).catch(console.log);
@@ -78,9 +78,21 @@ const authCallback = (event, context, callback) => {
 	const sonosAuthToken = Buffer.from(sonosClientID + ':' + sonosSecret).toString('base64');
 	console.log('Encoded token: ', sonosAuthToken);
 	const sonosTokenRedirect = encodeURIComponent('https://' + sonosCallbackID + '.execute-api.us-west-2.amazonaws.com/dev/token-callback');
-	const uriSonosCreateToken = 'https://api.sonos.com/login/v3/oauth/access?grant_type=authorization_code&code=' + sonosAuthCode + '&redirect_uri=' + sonosTokenRedirect;
+	// const uriSonosCreateToken = 'https://api.sonos.com/login/v3/oauth/access?grant_type=authorization_code&code=' + sonosAuthCode + '&redirect_uri=' + sonosTokenRedirect;
 	console.log('Posting Sonos create token request: ', uriSonosCreateToken);
-	//  axios.post(uriSonosCreateToken).then(console.log).catch(console.log);
+	
+	const uriSonosCreateToken = 'https://api.sonos.com/login/v3/oauth/access';
+	const bodyParameters{
+		grant_type: 'authorization_code',
+		code: sonosAuthCode,
+		redirect_uri: sonosTokenRedirect
+	}
+	const config = {
+    		headers: { Basic: token }
+	};
+	axios.post(uri, bodyParameters, config).then(console.log).catch(console.log);
+
+	
 	getToken(uriSonosCreateToken, sonosAuthToken);
 	// axios.get(uriSonosCreateToken).then(console.log).catch(console.log);
 	// const uriSonosAuth = 'https://api.sonos.com/login/v3/oauth?client_id=d313a2a0-960e-481f-9fc7-3c02e4366955&response_type=code&state=testState&scope=playback-control-all&redirect_uri=https%3A%2F%2Fr5twrfl7nd.execute-api.us-west-2.amazonaws.com%2Fdev%2Fauth-callback';
