dc7bce4568d59dc729edb79c4c675424327c3d93
davidhkates <davidhkates@yahoo.com>
2021-06-08 19:35:41 -0700

    Update handler.js

--- SonosControl/handler.js
+++ SonosControl/handler.js
@@ -75,7 +75,7 @@ const authCallback = (event, context, callback) => {
 	const sonosCallbackID = 'r5twrfl7nd';
 	const sonosSecret = '3acfdfd9-27c4-4a74-978d-e27fefa45bd2';
 	const sonosClientID = 'd313a2a0-960e-481f-9fc7-3c02e4366955';
-	const sonosAuthToken = btoa(sonosClientID + ':' + sonosSecret);
+	const sonosAuthToken = Buffer.from(sonosClientID + ':' + sonosSecret).toString('base64');
 	console.log('Encoded token: ', sonosAuthToken);
 	const sonosTokenRedirect = encodeURIComponent('https://' + sonosCallbackID + '.execute-api.us-west-2.amazonaws.com/dev/token-callback');
 	const uriSonosCreateToken = 'https://api.sonos.com/login/v3/oauth/access?grant_type=authorization_code&code=' + sonosAuthCode + '&redirect_uri=' + sonosTokenRedirect;
