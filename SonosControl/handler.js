54184b378b4dbba6ff152f70dd40dd2fcb70d53c
davidhkates <davidhkates@yahoo.com>
2021-06-10 15:04:35 -0700

    Update handler.js

--- SonosControl/handler.js
+++ SonosControl/handler.js
@@ -120,11 +120,7 @@ const authCallback = (event, context, callback) => {
 	})
 	*/
 	
-	axios.get('https://api.openweathermap.org/data/2.5/weather?q=Denver&appid=178796e24e49d001f0999f866eb7eb52', {
-		headers: {
-   			Authorization: 'Basic ' + sonosAuthToken,
-			'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'
-		})
+	axios.get('https://api.openweathermap.org/data/2.5/weather?q=Denver&appid=178796e24e49d001f0999f866eb7eb52')
 		.then(resp => {console.log('Axios openweather response: ', resp.data)})
 		.catch(console.log);
 		
