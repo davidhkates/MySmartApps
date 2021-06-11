7da0ef6c36a8db34438a6738af875afa7f4162a0
davidhkates <davidhkates@yahoo.com>
2021-06-10 20:13:51 -0700

    Update handler.js

--- SonosControl/handler.js
+++ SonosControl/handler.js
@@ -51,7 +51,8 @@ const authCallback = (event, context, callback) => {
 		}
 	};
 
-	axios.post(uriSonosCreateToken + '&' + postData, null, postHeaders)
+	// axios.post(uriSonosCreateToken + '&' + postData, null, postHeaders)
+	axios.post(uriSonosCreateToken, postData, postHeaders)
 		.then(resp => {console.log('Echo post data: ', resp.data)})
 		.catch(console.log);
 
