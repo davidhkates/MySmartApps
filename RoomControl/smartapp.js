bddce17f5ac7195b2c8d5cf2cc58465fad967d2a
davidhkates <davidhkates@yahoo.com>
2021-06-25 16:36:41 -0700

    Update smartapp.js

--- RoomControl/smartapp.js
+++ RoomControl/smartapp.js
@@ -19,7 +19,7 @@ async function getNextState( appId ) {
 		TableName: 'smartapp-state-machine',
   		Key: {
     			appId: { S: appId },
-			sequence: { N: '1' }
+			sequence: { N: 1 },
   		},
 	};
 
