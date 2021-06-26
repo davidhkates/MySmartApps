74ae5beabd6e9367256d917db1f72b6413548b40
davidhkates <davidhkates@yahoo.com>
2021-06-26 07:34:46 -0700

    Update smartapp.js

--- RoomControl/smartapp.js
+++ RoomControl/smartapp.js
@@ -33,7 +33,7 @@ async function getNextState( appId ) {
 				console.log("Error", err);
 				dbEnd = true;
 			} else {
-				if (data.length==0) {
+				if (data.Item.length==0) {
 					dbEnd = true;
 				} else {
 					console.log("State found", data.Item);
