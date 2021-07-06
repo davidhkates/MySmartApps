2df0e42915f7d6af3f96be839f005356c8114e07
davidhkates <davidhkates@yahoo.com>
2021-07-06 06:09:15 -0700

    Update smartapp.ts

--- RoomControl/smartapp.ts
+++ RoomControl/smartapp.ts
@@ -13,8 +13,7 @@ interface device {
 
 
 // global variables
-// let appSettings: any = {}
-const appSettings: any = {};
+let appSettings: any = {}
 
 //----------------------------------------------------------------------------------------
 // TODO: move routines to get settings values from DynamoDB database to katesthings
