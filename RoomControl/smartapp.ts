7f789e5784a9cf9e22089eed0d96e75990dc0440
davidhkates <davidhkates@yahoo.com>
2021-07-06 16:27:58 -0700

    Update smartapp.ts

--- RoomControl/smartapp.ts
+++ RoomControl/smartapp.ts
@@ -190,9 +190,9 @@ function getSettingValue(context, settingName) {
 	
 	// see if settings found in smartapp DynamoDB database
 	if (appSettings) {
-		console.log('Get setting value: ', settingName, appSettings.startTime, appSettings.getString(settingName));
+		console.log('Get setting value: ', settingName, appSettings.startTime, appSettings[settingName]);
 		// settingValue = appSettings.settingName;
-		settingValue = appSettings.getString(settingName);
+		settingValue = appSettings[settingName];
 	} else {
 		settingValue ??= context.configStringValue(settingName);
 	}
