b356f9149337eb3fba9ce6a5607648ee4dd40019
davidhkates <davidhkates@yahoo.com>
2021-07-09 06:06:07 -0700

    Update smartapp.ts

--- RoomControl/smartapp.ts
+++ RoomControl/smartapp.ts
@@ -124,7 +124,7 @@ function getSettingValue(context, settingName, bAppOnly) {
 
 // convert time in hhmm format to javascript date object
 function convertDateTime( hhmm ) {
-	const localToday = new Date().toLocaleString("en-US", {timeZone: "America/Denver"});
+	const localToday: any = new Date().toLocaleString("en-US", {timeZone: "America/Denver"});
 	const nHour: number = parseInt(hhmm.substr(0.2), 10);
 	const nMinute: number = parseInt(hhmm.substr(2,2), 10);
 	localToday.setHours(nHour);
