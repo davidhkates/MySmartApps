1cc2e8e4ee33bab6ae11a940f3f9125a70a7b8e2
davidhkates <davidhkates@yahoo.com>
2021-07-10 11:35:58 -0700

    Update smartapp.ts

--- RoomControl/smartapp.ts
+++ RoomControl/smartapp.ts
@@ -130,7 +130,7 @@ function convertDateTime( hhmm ) {
 		parseInt(hhmm.substr(0, 2), 10), parseInt(hhmm.substr(2, 2), 10));
 	const returnValue: Date = new Date(localTime.valueOf() + 7*60*60*1000);
 	const dt: Date = returnValue;
-	console.log('Converted date/time: ', returnValue.toLocaleString("en-US", {timeZone: "America/Denver"}));
+	console.log('Converted date/time: ', returnValue2.toLocaleString("en-US", {timeZone: "America/Denver"}));
 	const displayValue1: Date = new Date(Date.UTC(dt.getFullYear(), dt.getMonth(), dt.getDate(), dt.getHours(), dt.getMinutes(), dt.getSeconds()));
 	console.log('Display date, convertDateFromUTC: ', displayValue1);
 	const displayValue2: Date = new Date(dt.getUTCFullYear(), dt.getUTCMonth(), dt.getUTCDate(), dt.getUTCHours(), dt.getUTCMinutes(), dt.getUTCSeconds()); 
