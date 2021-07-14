8faeac5b2add81e203cf1ade055a317ed337005f
davidhkates <davidhkates@yahoo.com>
2021-07-13 17:54:06 -0700

    Update smartapp.ts

--- RoomControl/smartapp.ts
+++ RoomControl/smartapp.ts
@@ -110,6 +110,7 @@ function convertDateTime( hhmm ) {
 	const localDate: string = new Date().toLocaleString("en-US", {timeZone: "America/Denver", year: "numeric", month: "2-digit", day: "2-digit"});
 	const localTime: any = new Date(parseInt(localDate.substr(6, 4), 10), parseInt(localDate.substr(0, 2), 10)-1, parseInt(localDate.substr(3, 2), 10),
 		parseInt(hhmm.substr(0, 2), 10), parseInt(hhmm.substr(2, 2), 10));
+	console.log('Local time: ', localTime, localDate, tzOffset);
 	const returnValue: Date = new Date(localTime.valueOf() + tzOffset*60*60*1000);
 	console.log('Converted date/time: ', returnValue.toLocaleString("en-US", {timeZone: "America/Denver"}));
 	return returnValue;
