27a6ffb8f1d505212727a10ae3df122f294225a7
davidhkates <davidhkates@yahoo.com>
2021-07-22 20:25:28 -0700

    Update smartapp.ts

--- RoomControl/smartapp.ts
+++ RoomControl/smartapp.ts
@@ -145,8 +145,22 @@ async function writeLogEntry(logRecord) {
 	if (logSettings=='cw') {
 		console.log(logRecord);
 	} else {
+	
+		// var docClient = new aws.DynamoDB.DocumentClient();
+		// var docClient = new aws.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
+		// const dynamoDB = new aws.DynamoDB.DocumentClient({ region: "us-west-2" });
 		const dynamoDB = new aws.DynamoDB.DocumentClient();
 		const logTable = 'smartapp-circular-log';
+
+		/*
+		docClient.put({
+			Item: {
+				logItem: 1,
+				logRecord: 'See if this works'
+			},
+			TableName: logTable,
+		}).promise().then( data => console.log(data.Attributes)).catch(console.error);
+		*/
 
 		dynamoDB.get({
 			TableName: logTable,
@@ -155,10 +169,12 @@ async function writeLogEntry(logRecord) {
 			},
 		}).promise()
 		.then(function(data) {			
-			// console.log('Circular log data returned: ', data);
+			console.log('Circular log data returned: ', data);
+			// let logOffset: number = data.Items[0].logOffset;
+			// const maxRecords: number = data.Items[0].maxRecords;
 			let logOffset: number = data.Item.logOffset;
 			const maxRecords: number = data.Item.maxRecords;
-			// console.log('Circular log offset and maxRecords: ', logOffset, maxRecords);
+			console.log('Circular log offset and maxRecords: ', logOffset, maxRecords);
 
 			// write log record to next entry in circular table
 			dynamoDB.put({
@@ -168,23 +184,24 @@ async function writeLogEntry(logRecord) {
 				},
 				TableName: logTable,
 			}).promise()
-			.then(data => console.log(data.Attributes))
-			/*
-			.then(function(data) {
-				if (logOffset++ == maxRecords) { logOffset = 1 };
-				dynamoDB.put({
-					Item: {
-						logItem: 0,
-						logOffset: logOffset,
-					},
-					TableName: logTable,
-				})				
-			})
-			*/
+			.then( data => console.log(data.Attributes))
 			.catch(console.error);		
 		})		
 		.catch(console.error);
-	
+
+/*
+doSomething()
+.then(function(result) {
+  return doSomethingElse(result);
+})
+.then(function(newResult) {
+  return doThirdThing(newResult);
+})
+.then(function(finalResult) {
+  console.log('Got the final result: ' + finalResult);
+})
+.catch(failureCallback);		
+*/		
 
 		/*
 		console.log('Reading current circular log offset and maxRecords');
