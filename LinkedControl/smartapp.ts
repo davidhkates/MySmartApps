43f629c1ded8118e1e8ca316fd7ba75191d734b5
davidhkates <davidhkates@yahoo.com>
2021-10-10 09:03:33 -0600

    Try setMute to turn off sonos speaker

--- LinkedControl/smartapp.ts
+++ LinkedControl/smartapp.ts
@@ -406,7 +406,7 @@ module.exports = new SmartApp()
 		} else {
 			console.log('Turning off group immediately');
 			await context.api.devices.sendCommands(context.config.offGroup, 'switch', 'off');
-			await context.api.devices.sendComments(context.config.roomSpeakers, 'command', 'stop');
+			await context.api.devices.sendComments(context.config.roomSpeakers, 'mute', 'setMute');
 			// await context.api.devices.sendComments(context.config.roomSpeakers, 'supportedPlaybackCommand', 'stop');
 		}
 	}
