474f3e76332aeda45121021cbf1e974aff6e26f6
davidhkates <davidhkates@yahoo.com>
2021-10-22 07:20:09 -0600

    Removed smartdevice (unused)

--- MotionControl/smartapp.js
+++ MotionControl/smartapp.js
@@ -2,7 +2,7 @@
 const SmartApp = require('@smartthings/smartapp');
 
 // Install relevant SmartApp utilities
-const SmartSensor = require('@katesthings/smartcontrols');
+// const SmartDevice = require('@katesthings/smartdevice');
 const SmartUtils  = require('@katesthings/smartutils');
 
 
