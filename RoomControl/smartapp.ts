81bff37541b960601ba2aca9019e0c37f7c5be48
davidhkates <davidhkates@yahoo.com>
2021-11-05 20:59:27 -0600

    Separate on and monitor motion sensors

--- RoomControl/smartapp.ts
+++ RoomControl/smartapp.ts
@@ -58,6 +58,8 @@ module.exports = new SmartApp()
 	page.section('sensors', section => {
 		section.deviceSetting('roomMotion').capabilities(['motionSensor'])
 			.required(false).multiple(true).permissions('r');
+		section.deviceSetting('roomMotionOn').capabilities(['motionSensor'])
+			.required(false).multiple(true).permissions('r');
 		section.numberSetting('motionDelay').required(false).min(0);
 		section.deviceSetting('roomContacts').capabilities(['contactSensor'])
 			.required(false).multiple(true).permissions('r');
@@ -145,7 +147,7 @@ module.exports = new SmartApp()
 		    'switch', 'switch.off', 'groupOffHandler');
 
 		// initialize motion behavior
-		await context.api.subscriptions.subscribeToDevices(context.config.roomMotion,
+		await context.api.subscriptions.subscribeToDevices(context.config.roomMotionOn,
 		    'motionSensor', 'motion.active', 'motionStartHandler');
 		await context.api.subscriptions.subscribeToDevices(context.config.roomMotion,
 		    'motionSensor', 'motion.inactive', 'motionStopHandler');
