b3ed41beb0ef2abcebc66bcf675394688d8e6ea0
davidhkates <davidhkates@yahoo.com>
2021-11-09 05:58:39 -0700

    Update smartapp.ts

--- RoomControl/smartapp.ts
+++ RoomControl/smartapp.ts
@@ -148,7 +148,7 @@ module.exports = new SmartApp()
 
 		// initialize motion behavior
 		// await context.api.subscriptions.subscribeToDevices(context.config.roomMotionOn,
-		await context.api.subscriptions.subscribeToDevices(context.config.roomMotion2,
+		await context.api.subscriptions.subscribeToDevices(context.config.roomMotion,
 		    'motionSensor', 'motion.active', 'motionStartHandler');
 		await context.api.subscriptions.subscribeToDevices(context.config.roomMotion,
 		    'motionSensor', 'motion.inactive', 'motionStopHandler');
