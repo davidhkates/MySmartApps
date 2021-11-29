869a56de50eb472e4400ee5541460edac5feb530
davidhkates <davidhkates@yahoo.com>
2021-11-29 06:52:52 -0700

    Update smartapp.ts

--- FanControl/smartapp.ts
+++ FanControl/smartapp.ts
@@ -17,7 +17,7 @@ async function controlFan(context) {
 	// Initialize fan state variable
 	console.log('controlFan - starting control fan routine, initialize variables');
 
-	// Get indoor conditions and target values
+	// Get indoor conditions and target values.  TODO: logic for undefined with ??
 	const indoorTemp = await SmartDevice.getTemperature(context, 'tempSensor');
 	const indoorHumidity = await SmartDevice.getHumidity(context, 'humiditySensor');
 	const targetTemp = context.configNumberValue('targetTemp');
