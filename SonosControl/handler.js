3bf6e4cf8d4ae193708a7c41ee4afc0fad39e39b
davidhkates <davidhkates@yahoo.com>
2021-12-04 16:46:52 -0700

    Update handler.js

--- SonosControl/handler.js
+++ SonosControl/handler.js
@@ -1,15 +1,37 @@
 'use strict'
 
+// Install relevant node packages
+const axios = require("axios");
+
+// Sonos authorization callback
 exports.authCallback = (event, context, callback) => {
-	console.log('Code: ', event.queryStringParameters.code);
+	const authCode = event.queryStringParameters.code;
+	console.log('Code: ', authCode);
 
-	const message = {'message': 'Execution started successfully!'};
+	const message = {'message': 'Auth Code: ' + authCode};
 
+	/*
 	callback(null, {
 		statusCode: 200,
 		body: JSON.stringify(message),
 		headers: {'Content-Type': 'application/json'}
-	});	
+	});
+	*/
+	
+	const uriAuth = 'https://api.sonos.com/login/v3/oauth/access';
+	const request = 'grant_type=authorization_code&code=d37cca67-d509-4c04-9df4-49f8c6f0004b&redirect_uri=https%3A%2F%2FACME.example.com%3A7443%2Foauth%2Fv2%2Fclient%2Fauthcode';
+	const headers = { 
+		'Authorization': 'Basic my-token',
+		'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'
+	};
+	
+	/*
+	axios.post(uriAuth, article, { headers })
+		.then(response => element.innerHTML = response.data.id);	
+	*/
+	console.log('Making call to get token');
+	const response = await axios.post(uriAuth, request, headers);
+	console.log('Response data: ', response.data);
 };
 
 /*
