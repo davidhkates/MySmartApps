2037b08bcc435011cb0bd32ee0f95c070803ff81
davidhkates <davidhkates@yahoo.com>
2021-12-04 19:18:58 -0700

    Update handler.ts

--- SonosControl/handler.ts
+++ SonosControl/handler.ts
@@ -26,7 +26,8 @@ exports.authCallback = (event, context, callback) => {
 	console.log('Request: ', request);
 	// const request = 'grant_type=authorization_code&code=d37cca67-d509-4c04-9df4-49f8c6f0004b&redirect_uri=https%3A%2F%2FACME.example.com%3A7443%2Foauth%2Fv2%2Fclient%2Fauthcode';
 	const headers = { 
-		'Authorization': 'Basic ZDMxM2EyYTAtOTYwZS00ODFmLTlmYzctM2MwMmU0MzY2OTU1Ojk5NzdiNjVmLTMwM2QtNDQ4Ny1hOGY0LWRmNWE2ZDU1NTEzYQ==',
+//		'Authorization': 'Basic ZDMxM2EyYTAtOTYwZS00ODFmLTlmYzctM2MwMmU0MzY2OTU1Ojk5NzdiNjVmLTMwM2QtNDQ4Ny1hOGY0LWRmNWE2ZDU1NTEzYQ==',
+		'Authorization': 'Basic token:secret',
 		'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'
 	};
 	
