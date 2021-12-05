adb30cbb79414e4dddd9caefafa643eae9848899
davidhkates <davidhkates@yahoo.com>
2021-12-04 19:15:38 -0700

    Update handler.ts

--- SonosControl/handler.ts
+++ SonosControl/handler.ts
@@ -35,7 +35,7 @@ exports.authCallback = (event, context, callback) => {
 		.then(response => element.innerHTML = response.data.id);	
 	*/
 	console.log('Making call to get token');
-	const response = axios.post(uriAuth, request, {headers});
+	const response = axios.post(uriAuth, request, headers);
 	console.log('Response data: ', response.data);
 };
 
