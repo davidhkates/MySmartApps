8c6cb04b678ee481b13363666dad8e4a5588df2f
davidhkates <davidhkates@yahoo.com>
2021-12-06 18:54:39 -0700

    Get data from token request response

--- SonosControl/handler.ts
+++ SonosControl/handler.ts
@@ -3,8 +3,6 @@
 // Install relevant node packages
 const axios = require("axios");
 const qs = require("qs");
-// const formData = require("form-data");
-// const base64 = require("base64-js");
 
 // Sonos authorization callback
 exports.authCallback = (event, context, callback) => {
@@ -23,7 +21,6 @@ exports.authCallback = (event, context, callback) => {
 	*/
 	
 	if (authCode) {
-		// const formBody = 'grant_type=authorization_code&code=' + authCode + '&redirect_uri=https%3A%2F%2F00t156cqe1.execute-api.us-west-2.amazonaws.com%2Fdev%2Fauth-callback';
 		const url = 'https://api.sonos.com/login/v3/oauth/access';
 
 		const params = new URLSearchParams();
@@ -38,110 +35,14 @@ exports.authCallback = (event, context, callback) => {
 			}
 		}
 		
-		console.log('Sending POST request to retrieve token, parameters: ', params);
-		axios.post(url, params, config)
-			.then((result) => {
-				console.log('Success: ', result);
-			})
-			.catch((err) => {
-				console.log('Error: ', err);
-			})		
-			
-		/*
-		const response = fetch('https://api.sonos.com/login/v3/oauth/access', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
-				'Authorization': 'Basic ZDMxM2EyYTAtOTYwZS00ODFmLTlmYzctM2MwMmU0MzY2OTU1OjNhY2ZkZmQ5LTI3YzQtNGE3NC05NzhkLWUyN2ZlZmE0NWJkMg=='
-			}, 
-			body: formBody
-		});
-		
-		console.log('Success!  Results: ', response);
-		
-		/*
-		.then((response) => {
-			console.log('Success!  Results: ', response);
-		}).catch(err=>{console.error('Error: ', err)
-		});
-		*/
-		
-		/*
-		const form = new formData();
-		form.append('grant_type', 'authorization_code');
-		form.append('code', authCode);
-		form.append('redirect_uri', 'https%3A%2F%2F00t156cqe1.execute-api.us-west-2.amazonaws.com%2Fdev%2Fauth-callback');
-		*/
-		
-		// axios.post('https://example.com', form, { headers: form.getHeaders() })
-		
-		/*
-		axios.post('https://api.sonos.com/login/v3/oauth/access', formBody, {
-				headers: {
-					'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
-					'Authorization': 'Basic ZDMxM2EyYTAtOTYwZS00ODFmLTlmYzctM2MwMmU0MzY2OTU1OjNhY2ZkZmQ5LTI3YzQtNGE3NC05NzhkLWUyN2ZlZmE0NWJkMg=='
-				}
-			}
-		);
-		*/
-	
-		/*
-		const uriAuth = 'https://api.sonos.com/login/v3/oauth/access';
-		// const request = 'grant_type=authorization_code&code=' + authCode + '&redirect_uri=https%3A%2F%2F00t156cqe1.execute-api.us-west-2.amazonaws.com%2Fdev%2Ftoken-callback';
-		// console.log('Request: ', request);
-		// const request = 'grant_type=authorization_code&code=d37cca67-d509-4c04-9df4-49f8c6f0004b&redirect_uri=https%3A%2F%2FACME.example.com%3A7443%2Foauth%2Fv2%2Fclient%2Fauthcode';
-		
-		const postData = {
-			'grant_type': 'authorization_code',
-			'code': authCode,
-			'redirect_uri': 'https%3A%2F%2F00t156cqe1.execute-api.us-west-2.amazonaws.com%2Fdev%2Fauth-callback'
-		};
-
-		const postHeaders = {
-			headers: {
-				'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
-				'Authorization': 'Basic ZDMxM2EyYTAtOTYwZS00ODFmLTlmYzctM2MwMmU0MzY2OTU1OjNhY2ZkZmQ5LTI3YzQtNGE3NC05NzhkLWUyN2ZlZmE0NWJkMg=='
-			}
-		};
-
-		console.log('Making call to get token, headers:', postHeaders, ', data: ', postData);
-		axios.post(uriAuth, postData, postHeaders)
-			.then((res) => {
-				console.log("Response received: ", res);
-		})
-		.catch((err) => {
-			console.log("Error: ", err);
-		})
-		*/
+		axios.post(url, params, config).then((result) => {
+			console.log('Success!  Data: ', result.data);
+		}).catch((err) => {
+			console.log('Error: ', err);
+		})		
 	}
 };
 
-/*
-exports.authCallback = async function(event, context) {
-	console.log('Code: ', event.queryStringParameters.code);
-	return context.logStreamName;
-};
-	
-/*
-exports.handler =  async function(event, context) {
-  console.log("EVENT: \n" + JSON.stringify(event, null, 2))
-  return context.logStreamName
-}
-
-function authCallback(event, context, callback) {
-	// console.log('Auth made it: Event: ', event, '\nContext: ', context, '\nCallback: ', callback);
-	console.log('Code: ', event.queryStringParameters.code);
-    callback('success', 'error');
-	// return "Auth made it";
-};
-
-function authCallback(event, context) {
-	// console.log('Auth made it: Event: ', event, '\nContext: ', context, '\nCallback: ', callback);
-	console.log('Code: ', event.queryStringParameters.code);
-    // callback('success', 'error');
-	return context.logStreamName;
-};
-*/
 
 // Sonos authorization callback
 exports.tokenCallback = (event, context, callback) => {
@@ -157,15 +58,6 @@ exports.tokenCallback = (event, context, callback) => {
 	});
 };
 
-/*
-exports.handler = (event, context, callback) => {
-      mqfunc1(callback);
-};
-
-var mqfunc1 = function(callback) {
-    callback({'result': 'success'});
-};
-*/
 
 // export external modules
 // module.exports.authCallback  = authCallback
