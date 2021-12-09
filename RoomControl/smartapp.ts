59494c134140d30803f2cda71e85769fae3e022a
davidhkates <davidhkates@yahoo.com>
2021-12-09 07:48:30 -0700

    Update smartapp.ts

--- RoomControl/smartapp.ts
+++ RoomControl/smartapp.ts
@@ -193,7 +193,7 @@ module.exports = new SmartApp()
 			// console.log('controlSpeakers - speaker: ', speaker);
 			const command = 'pause';
 			const urlControl = '/groups/' + groupId + '/playback/' + command;
-			sonosControl.post(urlControl);
+			// sonosControl.post(urlControl);
 			
 		}
 	} catch(err) {
