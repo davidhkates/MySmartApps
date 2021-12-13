079e7618a72f41e47dfdf472a03de7188a5f5115
davidhkates <davidhkates@yahoo.com>
2021-12-13 07:19:24 -0700

    Update smartapp.ts

--- RoomControl/smartapp.ts
+++ RoomControl/smartapp.ts
@@ -349,7 +349,7 @@ module.exports = new SmartApp()
 	console.log('roomSwitchOffHandler - daysOfWeek: ', daysOfWeek, ', isDayOfWeek: ', SmartUtils.isDayOfWeek( daysOfWeek ) );
 	
 	const bTimeWindow = ( SmartUtils.inTimeContext( context, 'startTime', 'endTime' ) &&
-		SmartUtils.isDayOfWeek( context.configStringValue('daysOfWeek') ) ||
+		SmartUtils.isDayOfWeek( context.configStringValue('daysOfWeek') ) &&
 		!!(context.configStringValue('startTime')) ); 		
 		
 	console.log('roomSwitchOffHandler - in time window: ', bTimeWindow);
