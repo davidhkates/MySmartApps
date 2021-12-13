46a8e82fb07defff909eb8b7cdfca90140e757f8
davidhkates <davidhkates@yahoo.com>
2021-12-13 13:30:55 -0700

    Update handler.ts

--- SonosHandler/handler.ts
+++ SonosHandler/handler.ts
@@ -4,7 +4,8 @@
 'use strict'
 
 // Install relevant utilities
-const SmartState  = require('@katesthings/smartstate');
+const SmartState = require('@katesthings/smartstate');
+// const SmartSonos = require('@katesthings/smartsonos');
 
 // Install relevant node packages
 const axios = require("axios");
@@ -30,123 +31,66 @@ async function callSonosAPI( token_data, endpoint ) {
 		console.log('Error: ', err);
 	})		
 }
-*/
 
 async function putSonosData( key, value ) {
 	console.log('putSonosData - key: ', key, ', value: ', value);
 	SmartState.putHomeMode('niwot', 'sonos-' + key, value);
 }
+*/
 
 
 // Sonos authorization callback
 exports.authCallback = (event, context, callback) => {
 	const authCode = event.queryStringParameters.code;
-	// console.log('Event: ', event);
+	console.log('Event: ', event);
+	console.log('Context: ', context);
 	// console.log('Code: ', authCode);
-
-	/*
-	const message = {'message': 'Auth Code: ' + authCode};
-
-	callback(null, {
-		statusCode: 200,
-		body: JSON.stringify(message),
-		headers: {'Content-Type': 'application/json'}
-	});
-	*/
 	
-	// if (authCode) {
-	
-		const urlToken = 'https://api.sonos.com/login/v3/oauth/access';
+	const urlToken = 'https://api.sonos.com/login/v3/oauth/access';
 
-		const params = new URLSearchParams();
-		params.append('grant_type', 'authorization_code');
-		params.append('code', authCode);
-		params.append('redirect_uri', 'https%3A%2F%2F00t156cqe1.execute-api.us-west-2.amazonaws.com%2Fdev%2Fauth-callback');
-	
-		const config = {
-			headers: {
-				'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
-				'Authorization': 'Basic ZDMxM2EyYTAtOTYwZS00ODFmLTlmYzctM2MwMmU0MzY2OTU1OjNhY2ZkZmQ5LTI3YzQtNGE3NC05NzhkLWUyN2ZlZmE0NWJkMg=='
-			}
+	const params = new URLSearchParams();
+	params.append('grant_type', 'authorization_code');
+	params.append('code', authCode);
+	params.append('redirect_uri', 'https%3A%2F%2F00t156cqe1.execute-api.us-west-2.amazonaws.com%2Fdev%2Fauth-callback');
+
+	const config = {
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+			'Authorization': 'Basic ZDMxM2EyYTAtOTYwZS00ODFmLTlmYzctM2MwMmU0MzY2OTU1OjNhY2ZkZmQ5LTI3YzQtNGE3NC05NzhkLWUyN2ZlZmE0NWJkMg=='
 		}
+	}
+	
+	axios.post(urlToken, params, config).then((result) => {
+		console.log('Success!  Data: ', result.data);
 		
-		axios.post(urlToken, params, config).then((result) => {
-			console.log('Success!  Data: ', result.data);
-			
-			const sonosControl = axios.create({
-				baseURL: 'https://api.ws.sonos.com/control/api/v1',
-				timeout: 1000,
-				headers: {
-					'Content-Type': 'application/json',
-					'Authorization': result.data.token_type + ' ' + result.data.access_token
-				}
-			});
-			
-			// store tokens in DynamoDB home settings file
-			const token_data = result.data;
-			putSonosData( 'token-time', new Date() );
-			putSonosData( 'access-token', token_data.access_token );
-			putSonosData( 'refresh-token', token_data.refresh_token );
-			putSonosData( 'expires-in', token_data.expires_in );
+		const sonosControl = axios.create({
+			baseURL: 'https://api.ws.sonos.com/control/api/v1',
+			timeout: 1000,
+			headers: {
+				'Content-Type': 'application/json',
+				'Authorization': result.data.token_type + ' ' + result.data.access_token
+			}
+		});
+		
+		// store tokens in DynamoDB home settings file
+		const token_data = result.data;
+		SmartState.putSonosData( 'token-time', new Date() );
+		SmartState.putSonosData( 'access-token', token_data.access_token );
+		SmartState.putSonosData( 'refresh-token', token_data.refresh_token );
+		SmartState.putSonosData( 'expires-in', token_data.expires_in );
 
-			// callback(null, {body: JSON.stringify({'Households': idHousehold})});
-			callback(null, {
-				statusCode: 200,
-				body: JSON.stringify({
-					'access-token': token_data.access_token,
-					'refresh-token': token_data.refresh_token
-				}),
-				headers: {'Content-Type': 'application/json'}
-			});
-
-			/*
-			const households: any = getSonosData( sonosControl, 'households' );
-			const idHousehold = households.data.households[0].id;
-			const devices: any = getSonosData( sonosControl, 'households/' + idHousehold + '/groups');
-
-			callback(null, {
-				statusCode: 200,
-				body: JSON.stringify({'Households': idHousehold}),
-				headers: {'Content-Type': 'application/json'}
-			});
-			*/
-			
-			/*
-			const householdPromise = callSonosAPI( token_data, 'households' );
-			console.log('Households: ', householdPromise);
-			const householdList = async () => {
-				const listValue = await householdPromise;
-				console.log('Households: ', listValue);
-			};
-			
-			sonosControl.get('households').then((result) => {
-				const idHousehold = result.data.households[0].id;
-				console.log('Households: ', result.data);
-				putSonosData( 'household-id', idHousehold );
-				
-				
-				sonosControl.get('households/' + idHousehold + '/groups').then((result) => {
-					console.log('Groups: ', result.data.groups);
-					console.log('Stringified: ', JSON.stringify(result.data.groups));
-					// putSonosData( 'groups-json', JSON.stringify(result.data.groups) );
-					putSonosData( 'groups-json', result.data.groups );
-				});
-
-				// callback(null, {body: JSON.stringify({'Households': idHousehold})});
-				callback(null, {body: 'Success!  Tokens and groups stored in DynamoDB smartapp-home-settings'});
-				/*
-				callback(null, {
-					statusCode: 200,
-					body: JSON.stringify({'Households': idHousehold}),
-					headers: {'Content-Type': 'application/json'}
-				});
-			})
-			*/
-			
-		}).catch((err) => {
-			console.log('Error: ', err);
-		})		
-	// }
+		// callback(null, {body: JSON.stringify({'Households': idHousehold})});
+		callback(null, {
+			statusCode: 200,
+			body: JSON.stringify({
+				'access-token': token_data.access_token,
+				'refresh-token': token_data.refresh_token
+			}),
+			headers: {'Content-Type': 'application/json'}
+		});		
+	}).catch((err) => {
+		console.log('Error: ', err);
+	})		
 };
 
 
