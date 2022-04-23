1aa733886a5376a08bbc72714ec26b079348cbf0
davidhkates <davidhkates@yahoo.com>
2022-04-23 12:39:16 -0600

    Update smartapp.ts

--- HomeControl/smartapp.ts
+++ HomeControl/smartapp.ts
@@ -63,7 +63,9 @@ module.exports = new SmartApp()
 	// const locationList = context.api.locations.list; 
 	// console.log('homeControl - list of locations: ', locationList.toString());
 	console.log('homeControl - list of modes: ', context.api.modes.list.toString());
-	const modeList = await context.api.modes.list;
+	const modeList = await context.api.modes.list();
+	console.log('homeControl - list of modes: ', modeList);
+	const modeList = await context.api.modes.list('Niwot');
 	console.log('homeControl - list of modes: ', modeList);
 	// console.log('homeControl - list of locations: ', JSON.stringify(locationList));
 	
